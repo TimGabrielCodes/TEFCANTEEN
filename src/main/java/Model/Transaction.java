e53342c3432ba516980074fedff82578549cb1c0
TimGabriel1 <zinwotatimothy@gmail.com>
2021-10-27 09:48:12 +0100

    Set up Base Classes

--- src/main/java/Model/Transaction.java
+++ src/main/java/Model/Transaction.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
 /**
  *
  * @author mac
  */
+
+//A transaction is generated whenever food is sold and a receipt is printed
 public class Transaction {
     private int id;
     private int food_id;
